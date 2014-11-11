a8b2d9b553ab4fe1ed5ecfb0f2c8d64bac83d41b
Rodrigo Morais <morais.rm@gmail.com>
2014-11-11 17:05:41 -0200

    Open IndexedDB to save data from server.

---
+++ javascript/indexedDB.js
@@ -0,0 +1,37 @@
+﻿var localDB = (function () {
+    var db,
+        state = 'close';
+
+    var _isOpen = function () {
+        if (state === 'open') {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    var _init = function () {
+        if (window.indexedDB) {
+            var request = window.indexedDB.open("github", 1);
+
+            request.onerror = function (event) {
+                console.log('Error when open IndexedDB.');
+                state = 'close';
+            }
+
+            request.onsuccess = function (event) {
+                db = event.target.result;
+                state = 'open';
+            }
+        }
+    };
+
+    return {
+        init: _init,
+        isOpen: _isOpen
+    };
+
+})();
+
+localDB.init();
